d16c49985f8190489331c77225e696168b5cedbe
Erik Garcia Tacas <erik.fisi16@gmail.com>
2023-06-28 22:04:30 -0500

    feat: add context auth

---
+++ src/context/auth.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const AuthContext = React.createContext({});
+
+export const useAuth = () => {
+  return React.useContext(AuthContext);
+};
+
+const AuthProvider = (props) => {
+  const [user, setUser] = React.useState(null);
+  React.useEffect(() => {
+    if (!localStorage.getItem("user")) return;
+
+    setUser(JSON.parse(localStorage.getItem("user")));
+  }, []);
+
+  const updateUser = React.useCallback((dataUser) => {
+    setUser(dataUser);
+    localStorage.setItem("user", JSON.stringify(dataUser));
+  }, []);
+
+  const authProviderValue = React.useMemo(
+    () => ({
+      user,
+      updateUser,
+    }),
+    [user, updateUser]
+  );
+  return (
+    <AuthContext.Provider value={authProviderValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import AuthProvider from "./context/auth.jsx";
 
 const router = createHashRouter([
   {
@@ -13,6 +14,8 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
